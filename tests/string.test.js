69e271332e8d8b8fee9c2aadc63e314004038a3d
PalmTuomas <tuomas.palm@student.lab.fi>
2026-04-10 19:26:04 +0300

    Update string.test.js

--- tests/string.test.js
+++ tests/string.test.js
@@ -1,1 +1,21 @@
+import map from '../src/map.js';
+import reduce from '../src/reduce.js';
+import keys from '../src/keys.js';
 
+describe('map', () => {
+  test('maps array', () => {
+    expect(map([1,2,3], x => x * 2)).toEqual([2,4,6]);
+  });
+});
+
+describe('reduce', () => {
+  test('sum values', () => {
+    expect(reduce([1,2,3], (a,b) => a+b, 0)).toBe(6);
+  });
+});
+
+describe('keys', () => {
+  test('object keys', () => {
+    expect(keys({a:1,b:2})).toEqual(['a','b']);
+  });
+});
